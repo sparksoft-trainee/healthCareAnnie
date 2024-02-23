9e6c081977730753240f35779ae21961ce40db07
hannie404 <annieneshreend@gmail.com>
2024-02-23 20:31:26 +0800

    initialized amplify back-end

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "healthcare": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
